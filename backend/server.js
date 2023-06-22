b0110965591ff4537fed79bc916511bec0084ed1
blackboxHU <123558883+blackboxHU@users.noreply.github.com>
2023-06-22 21:37:54 +0200

    cleaning

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,19 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const upload = require("express-fileupload");
-const cors = require('cors');
+const cors = require("cors");
 
-const IMG_DIR = path.join(__dirname,"media")
-const DATA = path.join(__dirname,"authors","authors.json")
+const IMG_DIR = path.join(__dirname, "media");
+const DATA = path.join(__dirname, "authors", "authors.json");
 
 app.use(express.json());
 app.use(upload());
-app.use(cors())
+app.use(cors());
 
 app.get("/pictures/:filename", async (req, res) => {
   const filename = req.params.filename + ".jpg";
   const filePath = path.join(IMG_DIR, filename);
-  
+
   try {
     const imageBlob = await fs.promises.readFile(filePath);
     res.setHeader("Content-Type", "image/jpeg");
@@ -26,25 +26,24 @@ app.get("/pictures/:filename", async (req, res) => {
   }
 });
 
-
-app.get("/authors/", (req, res)=>{
-fs.readFile(DATA, "utf-8" ,(err, data)=>{
-    if(err){
-        res.status(404).send("Not found")
+app.get("/authors/", (req, res) => {
+  fs.readFile(DATA, "utf-8", (err, data) => {
+    if (err) {
+      res.status(404).send("Not found");
     }
     const author = JSON.parse(data);
     res.json(author);
-});
+  });
 });
 
-const date = new Date()
+const date = new Date();
 const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
+const month = String(date.getMonth() + 1).padStart(2, "0");
+const day = String(date.getDate()).padStart(2, "0");
+const hours = String(date.getHours()).padStart(2, "0");
+const minutes = String(date.getMinutes()).padStart(2, "0");
 
-    const formattedDate = `Uploaded at:${year}.${month}.${day} ${hours}:${minutes}`;
+const formattedDate = `Uploaded at:${year}.${month}.${day} ${hours}:${minutes}`;
 
 app.post("/authors/", (req, res) => {
   const formData = req.body;
@@ -76,11 +75,11 @@ app.post("/authors/", (req, res) => {
   const authorData = {
     id: currentMaxId,
     data: formData,
-    date: formattedDate
+    date: formattedDate,
   };
   authors.push(authorData);
 
-  const newName = currentMaxId+".jpg"
+  const newName = currentMaxId + ".jpg";
   fs.writeFile(DATA, JSON.stringify(authors, null, 2), "utf8", (err) => {
     if (err) {
       console.log(err);
@@ -99,48 +98,49 @@ app.post("/authors/", (req, res) => {
 app.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
 
-  fs.readFile(DATA, 'utf8', (err, data) => {
+  fs.readFile(DATA, "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      res.status(500).send('Hiba történt a fájl olvasása során.');
+      res.status(500).send("Hiba történt a fájl olvasása során.");
       return;
     }
 
     let jsonData = JSON.parse(data);
-    let entryToDelete = jsonData.find(entry => entry.id.toString() === id);
+    let entryToDelete = jsonData.find((entry) => entry.id.toString() === id);
 
     if (!entryToDelete) {
-      res.status(404).send('Nem található a megadott azonosítóval rendelkező bejegyzés.');
+      res
+        .status(404)
+        .send("Nem található a megadott azonosítóval rendelkező bejegyzés.");
       return;
     }
 
-    let updatedData = jsonData.filter(entry => entry.id.toString() !== id);
+    let updatedData = jsonData.filter((entry) => entry.id.toString() !== id);
     let updatedJsonData = JSON.stringify(updatedData);
 
-    fs.writeFile(DATA, updatedJsonData, 'utf8', err => {
+    fs.writeFile(DATA, updatedJsonData, "utf8", (err) => {
       if (err) {
         console.error(err);
-        res.status(500).send('Hiba történt a fájl írása során.');
+        res.status(500).send("Hiba történt a fájl írása során.");
         return;
       }
 
       const imagePath = path.join(IMG_DIR, `${id}.jpg`);
-      fs.unlink(imagePath, err => {
+      fs.unlink(imagePath, (err) => {
         if (err) {
           console.error(err);
-          res.status(500).send('Hiba történt a képfájl törlése során.');
+          res.status(500).send("Hiba történt a képfájl törlése során.");
           return;
         }
 
-        res.status(200).send('A bejegyzés és a képfájl sikeresen törölve lett.');
+        res
+          .status(200)
+          .send("A bejegyzés és a képfájl sikeresen törölve lett.");
       });
     });
   });
 });
 
-
-
-
-app.listen(9000,()=>{
-    console.log("Server is runing at http://localhost:9000")
-})
+app.listen(9000, () => {
+  console.log("Server is runing at http://localhost:9000");
+});
